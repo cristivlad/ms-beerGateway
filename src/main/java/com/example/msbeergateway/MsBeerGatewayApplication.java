01fdd7efee3148fa051deb784c09f6cb4f4155be
cristiii <cristian.vlad10@gmail.com>
2022-10-25 14:20:11 +0300

    init

---
+++ src/main/java/com/example/msbeergateway/MsBeerGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.example.msbeergateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsBeerGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsBeerGatewayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/msbeergateway/MsBeerGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.msbeergateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsBeerGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
